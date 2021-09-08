9f65996cc5153ca63875173d0899b37deaa0aa92
vitams <vmpartner@gmail.com>
2021-09-08 18:52:37 +0500

    v2

--- metric.go
+++ metric.go
@@ -1,6 +1,73 @@
 package metric
 
 import (
+	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/push"
+)
+
+type Metric struct {
+	name   string
+	pusher *push.Pusher
+}
+
+func New(name string, uri string) Metric {
+	return Metric{
+		name:   name,
+		pusher: push.New(uri, name),
+	}
+}
+
+func (m *Metric) Pusher() *push.Pusher {
+	return m.pusher
+}
+
+func (m *Metric) SafePush() {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	err := m.pusher.Add()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func (m *Metric) AddGauge(name string) prometheus.Gauge {
+	g := prometheus.NewGauge(prometheus.GaugeOpts{Name: name})
+	m.pusher.Collector(g)
+	return g
+}
+
+func (m *Metric) AddSummary(name string) prometheus.Summary {
+	g := prometheus.NewSummary(prometheus.SummaryOpts{Name: name})
+	m.pusher.Collector(g)
+	return g
+}
+
+func (m *Metric) AddHistogram(name string) prometheus.Histogram {
+	g := prometheus.NewHistogram(prometheus.HistogramOpts{Name: name})
+	m.pusher.Collector(g)
+	return g
+}
+
+func (m *Metric) AddCounter(name string) prometheus.Counter {
+	g := prometheus.NewCounter(prometheus.CounterOpts{Name: name})
+	m.pusher.Collector(g)
+	return g
+}
+
+/*
+func (m *Metric) StartGauge(name string) Gauge {
+	return Gauge{
+		name:  name,
+		start: time.Now(),
+	}
+}*/
+
+/*
+import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
@@ -263,3 +330,4 @@ func toFloat(value interface{}) float64 {
 
 	return res
 }
+*/
--- metric_test.go
+++ metric_test.go
@@ -1,41 +1,58 @@
 package metric_test
 
 import (
-	"github.com/moskvorechie/metric"
+	"github.com/moskvorechie/metric/v2"
 	"testing"
-	"time"
 )
 
-func init() {
-	metric.Init(metric.Params{
-		BufferWrn: 50,
-		Sleep:     1,
-		Url:       "http://127.0.0.1",
-		Test:      true,
-		App:       "test",
-	})
-}
+func TestMetric_New(t *testing.T) {
 
-func TestMetric_Stop(t *testing.T) {
-	x := metric.Start("test2")
-	time.Sleep(100 * time.Millisecond)
-	x.Stop()
-	if len(metric.Values()) <= 0 {
-		t.Fatal()
-	}
-	x.Records(100)
-	if len(metric.Values()) <= 1 {
-		t.Fatal()
-	}
-	if v := metric.Values()[1]; v.Value != 100 {
-		t.Fatal()
-	}
-	x.SubMetric("s1", 0.1)
-	if len(metric.Values()) <= 2 {
-		t.Fatal()
-	}
-	if v := metric.Values()[2]; v.Value != 0.1 {
-		t.Fatal()
-	}
-	time.Sleep(15 * time.Second)
+	m := metric.New("app_test", "http://127.0.0.1:30091")
+	defer m.SafePush()
+
+	records := m.AddGauge("db_backup_records_processed")
+	records.Set(101.00)
+
+	duration := m.AddGauge("db_backup_duration_seconds")
+	duration.Set(5.00)
+
+	//g := m.StartGauge("metric_1")
+	//time.Sleep(100 * time.Millisecond)
+
+	//g.StopAndPush()
+
+	//registry := prometheus.NewRegistry()
+	//registry.MustRegister(completionTime, duration, records)
+
+	//func init() {
+	//	//metric.Init(metric.Params{
+	//	//	BufferWrn: 50,
+	//	//	Sleep:     1,
+	//	//	Url:       "http://127.0.0.1",
+	//	//	Test:      true,
+	//	//	App:       "test",
+	//	//})
+	//}
+
+	//x := metric.Start("test2")
+	//time.Sleep(100 * time.Millisecond)
+	//x.Stop()
+	//if len(metric.Values()) <= 0 {
+	//	t.Fatal()
+	//}
+	//x.Records(100)
+	//if len(metric.Values()) <= 1 {
+	//	t.Fatal()
+	//}
+	//if v := metric.Values()[1]; v.Value != 100 {
+	//	t.Fatal()
+	//}
+	//x.SubMetric("s1", 0.1)
+	//if len(metric.Values()) <= 2 {
+	//	t.Fatal()
+	//}
+	//if v := metric.Values()[2]; v.Value != 0.1 {
+	//	t.Fatal()
+	//}
+	//time.Sleep(15 * time.Second)
 }
